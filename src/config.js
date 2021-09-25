4afe8f568b29d7c28f8f5bcaa64c1c20998befbb
Kesha Seyfert <kesha@MacBook-Pro-Kesha.local>
2021-09-25 19:07:24 +0300

    initial

--- src/config.js
+++
@@ -1,7 +0,0 @@
-const CLIENT_ID = '891242753948778526';
-const GUILD_ID = '891244538704855051';
-
-module.exports = {
-    GUILD_ID,
-    CLIENT_ID
-}
--- src/index.js
+++
@@ -1,8 +0,0 @@
-const {Bot} = require('./bot');
-
-const run = () => {
-    const bot = new Bot();
-    return bot.init();
-}
-
-run().then()
