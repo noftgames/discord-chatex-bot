24f56797ae39f7b263e87220692226a485ff4546
Kesha Seyfert <kesha@MacBook-Pro-Kesha.local>
2021-09-26 15:41:57 +0300

    f

--- bot/index.js
+++ bot/index.js
@@ -90,7 +90,8 @@ client.on('interactionCreate', async interaction => {
         data = '⛔️ The match is not finished yet'
         break;
       }
-      if (!bet.noft_id !== betBattle.scores[0].noft_id) {
+
+      if (bet.noft_id !== betBattle.scores[0].noft_id) {
         data = '⛔️ This bet hasn’t played. Try another one with /bet'
         break;
       }
