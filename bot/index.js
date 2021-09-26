4e5d9edeab7c73c6adac6ef3c2841e455cee2993
Kesha Seyfert <kesha@MacBook-Pro-Kesha.local>
2021-09-26 15:00:04 +0300

    f

--- bot/index.js
+++ bot/index.js
@@ -30,66 +30,66 @@ client.on('interactionCreate', async interaction => {
     case 'open_battles':
       const battles = await api.getOpenBattles();
       data = battles.length ?
-        battles.reduce((acc, cur) => acc + `Battle ID: ${cur.id} Status: ${cur.status} Nofts ${cur.nofts.join(' ')}\n`, '') :
-        'No battles available.';
+        battles.reduce((acc, cur) => acc + `⚔️ Battle ID: ${cur.id} Status: ${cur.status} Nofts ${cur.nofts.join(' ')}\n`, '') :
+        '⛔️ No battles available.';
       break;
     case 'all_battles':
       const allBattles = await api.getAllBattles();
       data = allBattles.length ?
-        allBattles.reduce((acc, cur) => acc + `Battle ID: ${cur.id} Status: ${cur.status} Nofts ${cur.nofts.join(' ')}\n`, '') :
-        'No battles available.';
+        allBattles.reduce((acc, cur) => acc + `⚔️ Battle ID: ${cur.id} Status: ${cur.status} Nofts ${cur.nofts.join(' ')}\n`, '') :
+        '⛔️ No battles available.';
       break;
     case 'battle':
       const battle = await api.getBattleById(interaction.options.getString('battle_id'));
       if (!battle) {
-        data = 'There is no match with this ID. Check the list of available matches with the /open_battles'
+        data = '⛔️ There is no match with this ID. Check the list of available matches with the /open_battles'
         break;
       }
       const bets = await api.getBetsByBattle(battle.id);
       const sum = bets.reduce((acc, cur) => acc + cur, 0);
       data = battle.status === 'FINISHED' ?
-        `${battle.id} Battle Status ${battle.status} Bets Pool: ${sum} Winner: ${battle.srores[0].noft_id} Record https://dev.noftgames.io/game` :
-        `${battle.id} Battle Status ${battle.status} Bets Pool: ${sum}`;
+        `⚔️ ${battle.id} Battle Status ${battle.status} Bets Pool: ${sum} Winner: ${battle.srores[0].noft_id} Record https://dev.noftgames.io/game` :
+        `⚔️ ${battle.id} Battle Status ${battle.status} Bets Pool: ${sum}`;
       break;
     case 'noft':
       const id = interaction.options.getString('noft_id');
       const noft = await api.getNoftById(id);
       data = noft ?
-        `Noft ${id} Abilities: (Vitality: ${noft.abilities.vitality} Vision: ${noft.abilities.vision} Power: ${noft.abilities.power} Agility: ${noft.abilities.agility} Speed: ${noft.abilities.speed}  Luck: ${noft.abilities.luck})` :
-        `There is no noft with this ID. Check the list of nofts with the /open_battles`
+        `${randomHeart()} Noft ${id} Abilities: (Vitality: ${noft.abilities.vitality} Vision: ${noft.abilities.vision} Power: ${noft.abilities.power} Agility: ${noft.abilities.agility} Speed: ${noft.abilities.speed}  Luck: ${noft.abilities.luck})` :
+        `⛔️ There is no noft with this ID. Check the list of nofts with the /open_battles`
       break;
     case 'bet':
       let result = await api.makeBet(interaction.user.id, interaction.options.getString('battle_id'), interaction.options.getString('noft_id'), interaction.options.getString('amount'))
-      data = 'The bets has been sent to processing. Confirm it with Chatex: ' + result.invoice_url;
+      data = '📝 The bets has been sent to processing. Confirm it with Chatex: ' + result.invoice_url;
       break;
     case 'my_bets':
       const myBets = await api.getBetsByUser(interaction.user.id);
       data = myBets.length ?
-        myBets.reduce((acc, cur) => acc + `Bet ID: ${cur.id}    |    Battle: ${cur.battle_id} Noft: ${cur.noft_id} Size: ${cur.amount} BTC\n`, '') :
-        "You haven't made any bets already. Make the first one with /bet";
+        myBets.reduce((acc, cur) => acc + `💰 Bet ID: ${cur.id}    |    Battle: ${cur.battle_id} Noft: ${cur.noft_id} Size: ${cur.amount} BTC\n`, '') :
+        "⛔️ You haven't made any bets already. Make the first one with /bet";
       break;
     case 'get_prize':
       const betId = interaction.options.getString('bet_id');
       const bet = await api.getBetById(betId);
       if (!bet) {
-        data = `There is no bet with this id. Check the list of your bets with /my_bets`;
+        data = `⛔️ There is no bet with this id. Check the list of your bets with /my_bets`;
         break;
       }
       const betBattle = await api.getBattleById(bet.battle_id);
       if (betBattle.status !== 'FINISHED') {
-        data = 'The match is not finished yet'
+        data = '⛔️ The match is not finished yet'
         break;
       }
       if (!bet.noft_id !== betBattle.scores[0].noft_id) {
-        data = 'This bet hasn’t played. Try another one with /bet'
+        data = '⛔️ This bet hasn’t played. Try another one with /bet'
         break;
       }
       data = await api.payBetPrize(betId);
-      data = 'Congrats! Chatex notificate you, when award will be proceed'
+      data = '✅ Congrats! Chatex notificate you, when award will be proceed'
 
       break;
     default:
-      data = 'Command not found';
+      data = '⛔️ Command not found';
       break;
   }
   await interaction.reply({content: data, ephemeral: true});
@@ -123,3 +123,9 @@ api.getEvents().on('bet_paid', async bet => {
   let channel = client.channels.cache.get('891253162017693750');
   await channel.send(`🤑 AWARD for battle ${bet.battle_id} HAVE BEEN PAID`);
 });
+
+function randomHeart() {
+  let arr = ['❤️', '🧡', '💛', '💚', '💙', '💜', '🤎'];
+  let rn = Math.round(Math.random() * arr.length);
+  return arr[rn];
+}
